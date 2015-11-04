12929fc503af1845ceecfc25999424638ec14316
unknown <hatish@gmail.com>
2015-11-04 21:18:13 +0200

    A little UI

--- RemoteFileBrowser-Angular2-TypeScript-Owin/WindowsService1/webdir/app.ts
+++ RemoteFileBrowser-Angular2-TypeScript-Owin/WindowsService1/webdir/app.ts
@@ -1,8 +1,22 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{myLocation}}</h2>
+    <p>Drives:</p>
+    <ul>
+      <li *ng-for="#drive of drives">
+        {{ drive }}
+      </li>
+    </ul>
+    `,
+    directives: [CORE_DIRECTIVES]
 })
 
-class AppComponent { }
+export class AppComponent {
+  title = 'Hatish Test';
+  myLocation = 'C:\\';
+  drives = ['C', 'D', 'E'];
+}
 bootstrap(AppComponent);
